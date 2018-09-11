9edff74bf1ddc3fd14dce6beb16583b41bd9027a
jose gutierrez <joseuribe0624@javerianacali.edu.co>
2018-09-10 19:10:59 -0500

    register file and test bench

--- RegisterFile.vhd
+++ RegisterFile.vhd
@@ -4,41 +4,33 @@ use IEEE.numeric_std.all;
 
 entity RegisterFile is
 port (
-	clk, rst: in std_logic;
-	registerWriteIn: in std_logic;
-	registerWriteTo: in std_logic_vector(2 downto 0);
-	registerWriteData: in std_logic_vector(15 downto 0);
-	registerReadAddress0: in std_logic_vector(2 downto 0);
-	registerReadData0: out std_logic_vector(15 downto 0);
-	registerReadAddress1: in std_logic_vector(2 downto 0);
-	registerReadData1: out std_logic_vector(15 downto 0)
+	clk: in std_logic;
+	registerWrite: in std_logic;
+	registerRead1: in std_logic_vector(4 downto 0);
+	registerRead2: in std_logic_vector(4 downto 0);
+	writeRegister: in std_logic_vector(4 downto 0);
+	registerWriteData: in std_logic_vector(31 downto 0);
+	registerReadData1: out std_logic_vector(31 downto 0);
+	registerReadData2: out std_logic_vector(31 downto 0)
 );
 end RegisterFile;
 
 architecture behavior of RegisterFile is  
-type reg_type is array (0 to 7) of std_logic_vector (15 downto 0);
-signal reg_array: reg_type;
-begin
-	process(clk, rst)
-	begin 
-	if(rising_edge(clk)) then
-		if(registerWriteIn = '1' ) then
-			reg_array(to_integer(unsigned(reg_write_dest))) <= registerWriteData;
-		end if;
-	elsif(rst='1') then
-		registerArray(0) <= x"0001";
-		registerArray(1) <= x"0002";
-		registerArray(2) <= x"0003";
-		registerArray(3) <= x"0004";
-		registerArray(4) <= x"0005";
-		registerArray(5) <= x"0006";
-		registerArray(6) <= x"0007";
-		registerArray(7) <= x"0008";
+	type reg_type is array (0 to 31) of std_logic_vector (31 downto 0);
+		--- llena la matriz de 0
+	signal reg_mem: reg_type:=((others => (others => '0')));
+	begin
+		---leo lo que hay en ese espacio de memoria
+		registerReadData1<= reg_mem(to_integer(unsigned(registerRead1)));
+		registerReadData2<= reg_mem(to_integer(unsigned(registerRead2)));
 	
-	end if;
-	end process;
-	registerReadData0 <= x"0000" when registerReadAddress0 = "000" else registerArray(to_integer(unsigned(registerReadAddress0)));
-	registerReadData1 <= x"0000" when registerReadAddress1 = "000" else registerArray(to_integer(unsigned(registerReadAddress1)));
-
+	process(clk)
+		begin
+      if rising_edge(CLK) then
+        if registerWrite = '1' then
+          reg_mem(to_integer(unsigned(writeRegister))) <= registerWriteData;
+        end if;
+      end if;
+	end process;	
 end behavior;
 
---
+++ tb_RegisterFile.vhd
@@ -0,0 +1,73 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tb_RegisterFile is
+end tb_RegisterFile;
+
+architecture behavioral of tb_RegisterFile is 
+	signal clk: std_logic := '0';
+	signal regWrite: std_logic := '1';
+	signal regRead1: std_logic_vector(4 downto 0);
+	signal regRead2: std_logic_vector(4 downto 0);
+	signal writeReg: std_logic_vector(4 downto 0);
+	signal regWriteData: std_logic_vector(31 downto 0);
+	signal regReadData1: std_logic_vector(31 downto 0);
+	signal regReadData2: std_logic_vector(31 downto 0); 
+	constant period : time := 10 ns;
+	component RegisterFile 
+		port (
+			clk: in std_logic;
+			registerWrite: in std_logic;
+			registerRead1: in std_logic_vector(4 downto 0);
+			registerRead2: in std_logic_vector(4 downto 0);
+			writeRegister: in std_logic_vector(4 downto 0);
+			registerWriteData: in std_logic_vector(31 downto 0);
+			registerReadData1: out std_logic_vector(31 downto 0);
+			registerReadData2: out std_logic_vector(31 downto 0)
+			);
+		end component;
+		begin
+			reg : RegisterFile port map(
+				clk => clk,
+				registerWrite => regWrite,
+				registerRead1 => regRead1,
+				registerRead2 => regRead2,
+				writeRegister => writeReg,
+				registerWriteData => regWriteData, 
+				registerReadData1 => regReadData1,
+				registerReadData2 => regReadData1
+			);
+			
+			Clock: process
+				begin
+					wait for 15 ns;
+					clk <= '1';
+					wait for 20 ns;
+					clk <= '0';
+					wait for 20 ns;
+					clk <= '1';
+			end process Clock;
+			
+			Write1: process
+				begin	
+					wait for 20 ns;
+					regWrite <= '1';
+					wait for 20 ns;
+					regWrite <= '0';
+			end process Write1;
+			
+			Addres: process
+				begin
+					wait for 20 ns;
+					writeReg <= x"0002";
+					wait for 20 ns;
+					regReadData1 <= writeReg;
+			end process Addres;
+			
+			Data: process
+				begin
+					wait for 20 ns;
+					regWriteData <= std_logic_vector(to_unsigned(114, regWriteData'length));
+			end process Data;
+end behavioral;
